4a485ca95364ab4b1e2dcd0a0b2fe39960b98670
Aaron_G <gauntta@gmail.com>
2019-04-11 19:17:45 -0500

    patient info model

--- models/clinic-visit.js
+++ models/clinic-visit.js
@@ -1,60 +1,60 @@
 // Create the visit model to be associated with a patient
 module.exports = function(sequelize, DataTypes) {
-    var Visit = sequelize.define("visit", {
-        reason: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        complaint: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        symptoms: {
-            general: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            eyes: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            earNoseThroat: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            cardio: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            gastro: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            musculoskeletal: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            skin: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            neuro: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-        },
-        vitals: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        }
-});
+  var Visit = sequelize.define("visit", {
+    reason: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    complaint: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    symptoms: {
+      general: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      eyes: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      earNoseThroat: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      cardio: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      gastro: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      musculoskeletal: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      skin: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      neuro: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    vitals: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
 
-Visit.associate = function(models) {
+  Visit.associate = function(models) {
     Visit.belongsTo(models.Patient, {
-        foreignKey: {
-            allowNull: false
-        }
+      foreignKey: {
+        allowNull: false
+      }
     });
-}
-return Visit;
-}
+  };
+  return Visit;
+};
