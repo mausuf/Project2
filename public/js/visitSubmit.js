c9d8325a573e4f7e39e133f6d6e0a61134f21424
Aaron_G <gauntta@gmail.com>
2019-04-12 13:05:49 -0500

    crash on node server fixed

--- public/js/visitSubmit.js
+++ public/js/visitSubmit.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 $("#records-btn").click(function() {
   $("#visit-form").modal("show");
 });
@@ -40,5 +41,3 @@ $("#info-submit").click(function() {
     .val()
     .trim();
 });
-
-
