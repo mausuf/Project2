c17b18d27aa04149a123cedc1787ef8c091f4384
Aaron_G <gauntta@gmail.com>
2019-04-11 19:35:32 -0500

    linting

--- models/patient.js
+++ models/patient.js
@@ -1,63 +1,63 @@
 // Create "Patient" model with basic info
-module.exports = function (sequelize, DataTypes) {
-    var Patient = sequelize.define("patient", {
-        // Indifiduat patient ID
-        patientID: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
+module.exports = function(sequelize, DataTypes) {
+  var Patient = sequelize.define("patient", {
+    // Indifiduat patient ID
+    patientID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    // Name of Patient
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    // Patient's address
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    // Patient's Date of Birth
+    DoB: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
+    },
+    // Patient's Phone Number
+    phoneNumber: Sequelize.DECIMAL,
+    // Patient's email
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [6, 128],
+          msg: "Email address must be between 6 and 128 characters in length"
         },
-        // Name of Patient
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        // Patient's address
-        address: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        // Patient's Date of Birth
-        DoB: {
-            type: Sequelize.DATEONLY,
-            allowNull: false
-        },
-        // Patient's Phone Number
-        phoneNumber: Sequelize.DECIMAL,
-        // Patient's email
-        email: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                len: {
-                    args: [6, 128],
-                    msg: "Email address must be between 6 and 128 characters in length"
-                },
-                isEmail: {
-                    msg: "Email address must be valid"
-                }
-            }
-        },
-        // Patient's gender
-        gender: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-        // Patient's ethnicity
-        ethnicity: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-        // Patient's Primary Care Doctor
-        PCP: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        meds: {
-            type: Sequelize.STRING,
-            allowNull: false
+        isEmail: {
+          msg: "Email address must be valid"
         }
-    });
-    return Patient;
+      }
+    },
+    // Patient's gender
+    gender: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    // Patient's ethnicity
+    ethnicity: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    // Patient's Primary Care Doctor
+    PCP: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    meds: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  });
+  return Patient;
 };
