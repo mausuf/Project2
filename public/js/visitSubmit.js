dded66087f490aadae17b768ae68c6d68697c1e3
Aaron_G <gauntta@gmail.com>
2019-04-12 16:10:06 -0500

    fixes

--- public/js/visitSubmit.js
+++ public/js/visitSubmit.js
@@ -2,49 +2,6 @@ $("#records-btn").click(function() {
   $("#visit-form").modal("show");
 });
 
-<<<<<<< HEAD
-$("#info-submit").click(function() {
-  var name = $("#log-name")
-    .val()
-    .trim();
-  var phone = $("#log-phone-number")
-    .val()
-    .trim();
-  var email = $("#log-email")
-    .val()
-    .trim();
-  var dob = $("#log-dob")
-    .val()
-    .trim();
-  var address1 = $("#log-address")
-    .val()
-    .trim();
-  var address2 = $("#log-address2")
-    .val()
-    .trim();
-  var city = $("#log-city")
-    .val()
-    .trim();
-  var state = $("#log-state")
-    .val()
-    .trim();
-  var zip = $("#log-zip")
-    .val()
-    .trim();
-  var gender = $("#log-gender")
-    .val()
-    .trim();
-  var ethnicity = $("#log-ethnicity")
-    .val()
-    .trim();
-  var primaryCare = $("#log-dr")
-    .val()
-    .trim();
-});
-
-
-=======
 $("#wwwmb-btn").click(function() {
   $("#wwwmb-form").modal("show");
 });
->>>>>>> 55001790381bf672e24be56338587d20b5c0ca06
