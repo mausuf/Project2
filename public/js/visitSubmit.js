ce8ab72707dc28488582a9762778b4a9e51b4a4b
Aaron.Gauntt <gauntta@gmail.com>
2019-04-12 21:54:57 -0500

    revised info submission

--- public/js/visitSubmit.js
+++ public/js/visitSubmit.js
@@ -1,8 +1,29 @@
 /* eslint-disable no-unused-vars */
-$("#records-btn").click(function() {
-  $("#visit-form").modal("show");
-});
+$(document).ready(function() {
+  $("#records-btn").click(function() {
+    $("#visit-form").modal("show");
+  });
 
-$("#wwwmb-btn").click(function() {
-  $("#wwwmb-form").modal("show");
+  $("#wwwmb-btn").click(function() {
+    $("#wwwmb-form").modal("show");
+  });
+
+  $("#info-submit").click(function() {
+    //Patient data object
+    var patientData = {
+      name: $("#log-name").val(),
+      number: $("#log-phone-number").val(),
+      dob: $("#log-dob").val(),
+      address: {
+        add1: $("#log-address").val(),
+        add2: $("#log-address2").val(),
+        city: $("#log-city").val(),
+        state: $("#log-state").val(),
+        zip: $("#log-zip".val())
+      },
+      gender: $("#log-gender").val(),
+      ethnicity: $("#log-ethnicity").val(),
+      primaryPhy: $("#log-dr").val()
+    };
+  });
 });
